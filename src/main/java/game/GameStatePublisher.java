7f4c4c9d88a3410ac0b4beb7a962d49786d9b5f5
Joe Westwell <westwelljoe@gmail.com>
2024-03-22 11:26:10 +0000

    Subscriber interface and publisher class for observer patten. Used to track and publish events based on hp, gold, etc

---
+++ src/main/java/game/GameStatePublisher.java
@@ -0,0 +1,53 @@
+package game;
+
+import java.util.ArrayList;
+
+// Publishes events based on the values of hp, gold
+public class GameStatePublisher {
+
+    private ArrayList<GameSubscriber> subscribers;
+    private int hp;
+    private int gold;
+
+    public GameStatePublisher(ArrayList<GameSubscriber> subscribers, int hp, int gold) {
+        this.subscribers = subscribers;
+        this.hp = hp;
+        this.gold = gold;
+    }
+
+    public void subscribe(GameSubscriber s) {
+        subscribers.add(s);
+    }
+
+    public void unsubscribe(GameSubscriber s) {
+        int i = subscribers.indexOf(s);
+        if (subscribers.indexOf(i) >= 0) {
+            subscribers.remove(i);
+        }
+    }
+    
+    public void notify(String event){
+        for (GameSubscriber sub : subscribers) {
+            sub.update(event);
+        }
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+    
+    
+
+}
---
+++ src/main/java/game/GameSubscriber.java
@@ -0,0 +1,6 @@
+package game;
+
+// Interface for towers, enemies, etc. Subscribers will be notifed when events such as low hp are triggered.
+public interface GameSubscriber {
+    void update(String event);
+}
