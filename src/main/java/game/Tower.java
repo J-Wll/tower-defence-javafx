ff9c520a8c443fccb7c35264e95cd2f4117d5e0d
Joe Westwell <westwelljoe@gmail.com>
2024-03-22 11:32:58 +0000

    Basic tower class

---
+++ src/main/java/game/Tower.java
@@ -0,0 +1,21 @@
+package game;
+
+public class Tower implements GameSubscriber{
+    private int baseDamage;
+    private int damage;
+    private int baseAttackSpeed;
+    private int attackSpeed;
+    
+    public void update(String event){
+//        Temporary attack boost
+        if ("lowHP".equals(event)){
+            damage *= 1.5;
+            attackSpeed *= 1.5;
+        }
+//        Reset attack
+        if ("normalHP".equals(event)){
+            damage = baseDamage;
+            attackSpeed = baseAttackSpeed;
+        }
+    }
+}
